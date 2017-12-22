0d8b765a5894fac2858c4235f95969cd64c05366
Unknown <maxim7853@gmail.com>
2017-12-22 10:32:46 +0300

    3

--- praline/prod/js/scrypt.js
+++ praline/prod/js/scrypt.js
@@ -1,50 +1,18 @@
-Vue.component('modal-bg', {
-  template: '#modal-bg'
-})
-
-Vue.component('modal-1', {
-  template: '#modal-1'
-})
-
-Vue.component('modal-2', {
-  template: '#modal-2'
-})
-
-Vue.component('modal-3', {
-  template: '#modal-3'
-})
-
-Vue.component('modal-4', {
-  template: '#modal-4'
-})
-
 Vue.use(VueAwesomeSwiper)
-
 new Vue({
-
   el: '#app',
   data: {
-    modal_bg: false,
-    modal_1: false,
-    modal_2: false,
-    modal_3: false,
-    modal_4: false,
-    swiperOptionA: {
-      speed: 700,
+    modal_bg: 0,
+    modal: 0,
+    slider: {
+      speed: 600,
       loop: true,
-       pagination: {
-         el: '.swiper-pagination'
-       },
        navigation: {
          nextEl: '.swiper-button-next',
          prevEl: '.swiper-button-prev'
        }
     },
   },
-  methods: {
-    reset_form: function() {
-      event.target.reset()
-    }
+  methods: {reset_form:function(){event.target.reset()}
   },
-
 })
